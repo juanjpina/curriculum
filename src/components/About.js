80d0618e6a708655821bf5b2d6c6044edd080e6c
juanjpina <juanjpina@gmail.com>
2022-05-04 14:38:23 +0200

    04/05-01

--- src/components/About.js
+++ src/components/About.js
@@ -8,8 +8,10 @@ export const About = () => {
 
 
         <div className='about__content'>
-            <p className='about__text'>Hola soy un desarrollador web y web móvil (fullstack) he desarrollado varios proyectos y sigo creando otros nuevos.<br />
-                Soy una persona resolutiva y creativa, dentro de un espíritu de equipo.
+            <p className='about__text'>Hola soy un desarrollador web y web móvil (fullstack) junior, he desarrollado varios proyectos y sigo creando otros nuevos.<br />
+                Soy una persona resolutiva y creativa, dentro de un espíritu de equipo.<br />
+                Mi objetivo es la seguridad y la minima complejidad en el desarrollo de codigo.<br />
+                Estoy siempre a su disposción.
 
             </p>
         </div >
--- src/components/Project.js
+++ src/components/Project.js
@@ -5,19 +5,29 @@ export const Project = () => {
         <div className='project__content animate__animated animate__fadeIn'>
             <div className='project__reducer'>
 
-                <a href="https://drive.google.com/file/d/1VGEaZEqKtCsQQMqEtOAqMEBOuBJ_TR43/view?usp=sharing" download target="_blank" title="Projecto Android" rel="noreferrer">
-                    <div className='project__android project__border'>
+                <a href="https://drive.google.com/file/d/1VGEaZEqKtCsQQMqEtOAqMEBOuBJ_TR43/view?usp=sharing" download target="_blank" rel="noreferrer">
+                    <div className='project__border'>
+                        <p className='project__img'>
+                            Proyecto Android/Java
+                            Versión Alfa
+                        </p>
                         <img className='project__imb-bread' src="\assets\img\bread150.png" width="15" alt="aplicaciom movil" />
                         <p>Prototipo aplicación móbil</p>
                     </div>
                 </a>
             </div>
             <div className='project__reducer'>
-
-                <div className='project__border'>
-                    <img src='\assets\img\car.jpg' width="150" alt="aplicacion web" />
-                    <p>Aplicación web</p>
-                </div>
+                <a href='http://car.juanjpina.com/' download target="_blank" rel="noreferrer">
+                    <div className='project__border'>
+                        <p className='project__img'>
+                            Proyecto web/móvil
+                            PHP/SQL<br />
+                            Versión Alfa
+                        </p>
+                        <img src='\assets\img\car.jpg' width="150" alt="aplicacion web" />
+                        <p>Aplicación web</p>
+                    </div>
+                </a>
             </div>
         </div>
     )
